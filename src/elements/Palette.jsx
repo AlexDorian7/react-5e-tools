6f27de641849fe0d40dfbd54a8f1a39c30af94a7
Alex <alexdollar3@gmail.com>
2024-05-04 21:36:20 -0500

    Changes to be committed: 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/main.js 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/elements/Palette.jsx 	new file:   src/elements/Test.jsx 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/logo.svg 	new file:   src/reportWebVitals.js 	new file:   src/setupTests.js

---
+++ src/elements/Palette.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export const palettes = {
+    DEFAULT: 0,
+
+
+};
+
+export default function PalleteSystem() {
+    const [palette, setPalette] = useState(0);
+}
---
+++ src/elements/Test.jsx
@@ -0,0 +1,7 @@
+export default function TestElement() {
+    return (
+        <>
+            <p>I am a very cool <code>p</code> tag!</p>
+        </>
+    );
+}
